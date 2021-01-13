7d77521202f1b59e02b601263a376bb90807f624
Aman Soni <65482419+gitaman8481@users.noreply.github.com>
2021-01-13 22:46:23 +0530

    Netflic Ui clone mod 1

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone_/screens/nav_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Netflix UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: NavScreen(),
+    );
+  }
+}
